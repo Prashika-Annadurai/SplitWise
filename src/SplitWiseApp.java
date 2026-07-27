1949a22cd0b4df956514cae5b822c6617dbf9bae
Prashii <prashikaannadurai3918@gmail.com>
2026-07-27 23:09:31 +0530

    1st commit

---
+++ src/SplitWiseApp.java
@@ -0,0 +1,7 @@
+public class SplitWiseApp{
+    public static void main(String args[]){
+        System.out.println("===SPLITWISE CONSOLE===");
+        System.out.println("Track shared expenses with Friends");
+        System.out.println("Ready.More features coming in the next lesson.");
+    }
+}
