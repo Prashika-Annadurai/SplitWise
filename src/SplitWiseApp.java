5f4fdc3624ef1005044464356058a22221a030ea
Prashii <prashikaannadurai3918@gmail.com>
2026-07-27 23:19:39 +0530

    2nd commit

--- src/SplitWiseApp.java
+++ src/SplitWiseApp.java
@@ -3,5 +3,9 @@ public class SplitWiseApp{
         System.out.println("===SPLITWISE CONSOLE===");
         System.out.println("Track shared expenses with Friends");
         System.out.println("Ready.More features coming in the next lesson.");
+        double totalAmount=500.00;
+        int numFriends=3;
+        System.out.println(totalAmount);
+        System.out.println(numFriends);
     }
 }
